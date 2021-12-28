cf69e452e6b649962ac90ca752cb965344d84ed9
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2021-12-28 10:02:05 -0800

    version1.0

---
+++ proyecto9.9/proyecto9.9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto9._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int numero = 8;
+                while (numero<=500)
+                {
+                    if (numero == 496)
+                    {
+                        Console.Write(" "+numero);
+                    }
+                    else
+                    {
+                        Console.Write(" " + numero + " -");                       
+                    }
+                    numero = numero + 8;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ha introducido un dato invalido");
+                throw;
+            }
+            Console.ReadKey();
+        }
+    }
+}
